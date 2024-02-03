bafc102cb3e8e37ff4afa8d89625d56331cccb50
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2024-02-03 17:30:38 +0200

    last changes in first

--- main.js
+++ main.js
@@ -14,7 +14,25 @@ hide_display.forEach((item)=>{
 console.log(moredetals)
 moredetals.forEach((item,i)=>{
     item.href=`./ntlp/cases/ntp.html?id=${i+1}`
-    console.log(item)
+   
   })
-
-
+  let popUpOverlay=document.querySelectorAll(".popUpOverlay")
+  let close=document.querySelectorAll(".popUpOverlay .close")
+  
+let popUpclick=document.querySelectorAll(".popUpclick")
+popUpclick.forEach((item,i)=>{
+item.addEventListener(('click'),()=>{
+    popUpOverlay[i].style.opacity="1"
+    popUpOverlay[i].style.visibility="visible"
+    popUpOverlay[i].style.zIndex="9999"
+})
+})
+close.forEach((item,i)=>{
+item.addEventListener(('click'),()=>{
+    popUpOverlay[i].style.opacity="0"
+    popUpOverlay[i].style.visibility="hidden"
+    // popUpOverlay[i].style.zIndex=""
+})
+})
+ 
+  
